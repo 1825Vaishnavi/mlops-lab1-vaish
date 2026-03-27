f3a66adc8f2423ce799bb04506c27d3abfd8370e
Vaishnavi <1825Vaishnavi@users.noreply.github.com>
2026-03-27 19:32:46 -0400

    Cloud Run Lab - Vaishnavi Gajarla modifications

--- Labs/GCP_Labs/Cloud_Runner_Labs/Begineer_Lab/app.py
+++ Labs/GCP_Labs/Cloud_Runner_Labs/Begineer_Lab/app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return "Welcome to My Cloud Run App! 🚀"
+
+@app.route('/about')
+def about():
+    return "This app is built by [VAISHNAVI GAJARLA] and deployed on Google Cloud Run!"
+
+@app.route('/health')
+def health():
+    return "App is running healthy "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
